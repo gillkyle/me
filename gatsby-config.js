2ce330d18067ee114f09b430814241c7d85d8159
Kyle Gill <kylerobertgill@gmail.com>
2019-04-29 12:16:06 -0600

    chore: add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-favicon',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
